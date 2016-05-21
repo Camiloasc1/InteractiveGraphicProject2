58008a1905aea108c4d0913aca8a9760c9c4eed8
caasanchezcu <caasanchezcu@unal.edu.co>
2016-05-21 13:40:48 -0500

    Connections between stations in map and random start point.

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 var player;
+var currentStation;
 
 function preload() {
     game.load.image('map', 'assets/Map.jpg');
@@ -13,19 +14,33 @@ function preload() {
 }
 
 function create() {
+    //Set world size
+    var image = game.cache.getImage('map');
+    game.world.setBounds(0, 0, image.width, image.height);
+
+    //Background map
     game.add.image(0, 0, 'map');
+
+    //Player sprite
     player = game.add.sprite(0, 0, 'bus');
     player.scale.setTo(0.05, 0.05);
     player.anchor.set(0.5, 0.5);
-    var image = game.cache.getImage('map');
-    game.world.setBounds(0, 0, image.width, image.height);
     game.camera.follow(player);
+
+    //Buttons for each station
     var button;
     for (var i = 0; i < stations.length; i++) {
         button = game.add.button(stations[i].x - 10, stations[i].y - 10, null, onButtonPressed, this);
         button.height = 20;
         button.width = 20;
+        button.station = stations[i];
+        stations[i].button = button;
     }
+
+    //Start at random station
+    currentStation = stations[game.rnd.integerInRange(0, stations.length - 1)];
+    player.x = currentStation.x;
+    player.y = currentStation.y;
 }
 
 function update() {
@@ -35,15 +50,21 @@ function update() {
 function render() {
 
 }
-
 function onButtonPressed(button) {
-    this.game.add.tween(player)
-        .to(
-            {x: button.x + 10, y: button.y + 10},
-            1000,
-            Phaser.Easing.Sinusoidal.InOut,
-            true,
-            0,
-            0
-        );
+    var targetStation;
+    targetStation = button.station;
+
+    //Move to target if is connected
+    if (currentStation.connected.indexOf(targetStation.name) != -1) {
+        currentStation = targetStation;
+        this.game.add.tween(player)
+            .to(
+                {x: targetStation.x, y: targetStation.y},
+                1000,
+                Phaser.Easing.Sinusoidal.InOut,
+                true,
+                0,
+                0
+            );
+    }
 }
