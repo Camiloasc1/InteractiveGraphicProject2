d99aefb05575a4f6b2cf3eb706c7e91f9227c1ce
caasanchezcu <caasanchezcu@unal.edu.co>
2016-05-20 22:35:54 -0500

    Map added.

--- js/game.js
+++ js/game.js
@@ -16,14 +16,27 @@ function create() {
     game.add.image(0, 0, 'map');
     player = game.add.sprite(0, 0, 'bus');
     player.scale.setTo(0.05, 0.05);
+    player.anchor.set(0.5, 0.5);
     var image = game.cache.getImage('map');
     game.world.setBounds(0, 0, image.width, image.height);
     game.camera.follow(player);
+    var button;
+    for (var i = 0; i < stations.length; i++) {
+        button = game.add.button(stations[i].x - 10, stations[i].y - 10, null, onButtonPressed, this);
+        button.height = 20;
+        button.width = 20;
+    }
 }
 
 function update() {
+
 }
 
 function render() {
 
+}
+
+function onButtonPressed(button) {
+    player.x = button.x + 10;
+    player.y = button.y + 10;
 }
